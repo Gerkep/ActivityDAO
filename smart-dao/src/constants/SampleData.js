4716f412820ed65ea617d0ccf9e9341ca23ddadf
Gerkep <piotrg2003@gmail.com>
2022-04-23 18:19:21 +0200

    sample data update

--- smart-dao/src/constants/SampleData.js
+++ smart-dao/src/constants/SampleData.js
@@ -1,6 +1,6 @@
 export const Proposals = [
     {
-        id : 0,
+        id : '0x453regq4534gq5t3',
         proposer : '0x1213212312312',
         location : 'Malaysia',
         times : [
@@ -10,7 +10,7 @@ export const Proposals = [
         deadline : 1650733871
     },
     {
-        id : 0,
+        id : '0x789534072fwedfer',
         proposer : '0x1213212312312',
         location : 'Amsterdam',
         times : [
@@ -20,7 +20,7 @@ export const Proposals = [
         deadline : 1650733871
     },
     {
-        id : 0,
+        id : '0x45wgw4hg5h6wgh4w',
         proposer : '0x1213212312312',
         location : 'Madrid',
         times : [
@@ -30,9 +30,19 @@ export const Proposals = [
         deadline : 1650733871
     },
     {
-        id : 0,
+        id : '0x43t55gy4gterhhgetgwrt',
         proposer : '0x1213212312312',
         location : 'Paris',
+        times : [
+            '11:00pm - 12pm',
+            '9:00am - 11:00am'
+        ],
+        deadline : 1650733871
+    },
+    {
+        id : '0x34gtb45g44gwrt45g5',
+        proposer : '0x1213212312312',
+        location : 'Madrid',
         times : [
             '11:00pm - 12pm',
             '9:00am - 11:00am'
