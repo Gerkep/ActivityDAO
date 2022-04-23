06b4200f8bca1b7670473a50fe7e7d6eba0ece6a
CarlosR <carlos.ramos@grupoimportia.com>
2022-04-23 16:25:23 +0200

    (add) : sample data for proposal

---
+++ smart-dao/src/constants/SampleData.js
@@ -0,0 +1,43 @@
+export const Proposals = [
+    {
+        id : 0,
+        proposer : '0x1213212312312',
+        location : 'Malaysia',
+        times : [
+            '11:00pm - 12pm',
+            '9:00am - 11:00am'
+        ],
+        deadline : 1650733871
+    },
+    {
+        id : 0,
+        proposer : '0x1213212312312',
+        location : 'Amsterdam',
+        times : [
+            '11:00pm - 12pm',
+            '9:00am - 11:00am'
+        ],
+        deadline : 1650733871
+    },
+    {
+        id : 0,
+        proposer : '0x1213212312312',
+        location : 'Madrid',
+        times : [
+            '11:00pm - 12pm',
+            '9:00am - 11:00am'
+        ],
+        deadline : 1650733871
+    },
+    {
+        id : 0,
+        proposer : '0x1213212312312',
+        location : 'Paris',
+        times : [
+            '11:00pm - 12pm',
+            '9:00am - 11:00am'
+        ],
+        deadline : 1650733871
+    },
+    
+]
