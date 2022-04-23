354b94eb7e447f31972fa55256de2a0ab151bf71
Gerkep <piotrg2003@gmail.com>
2022-04-23 19:58:15 +0200

    update

--- smart-dao/src/Pages/Activity.js
+++ smart-dao/src/Pages/Activity.js
@@ -4,11 +4,18 @@ import VotingRow from "../components/VotingRow";
 
 
 import "../style/daoDashboard.css"
-import { Proposals
-} from "../constants/SampleData";
+import { Proposals} from "../constants/SampleData";
 const Activity = () => {
   const {id} = useParams();
 
+  const renderProposals = () => {
+    return Proposals.map(item => {
+      return(
+        <VotingRow location={item.location} proposer={item.proposer} timeOptions={item.times} remainingTime={item.deadline}> </VotingRow>
+      )
+    })
+  }
+  
   return (
     <div>
       {id}
@@ -16,18 +23,7 @@ const Activity = () => {
       <button className="create-idea-btn">Propose an idea</button>
       <h1 className="page-name">Barcelona Soccer Team</h1>
       <div className="votings-container">
-        {
-          Proposals.map(item => {
-            return(
-              <VotingRow location={item.location} proposer={item.proposer} timeOptions={item.times} remainingTime={item.deadline}> </VotingRow>
-            )
-          })
-        }
-          {/* <VotingRow name="Match Sunday at 5PM?" description="regreg eger5g4 45g 4etwhgwtrhgsf hgfdh gnsfbdg rsthsryn s sdfg snhyrtsh sfg" votes={13} votesGoal={21}/>
-          <VotingRow name="Basketball tomorrow?" description="regrehgsf hgfdh gnsfbdg rsthsryn s sdfg snhyrtsh sfg" votes={3} votesGoal={11}/>
-          <VotingRow name="Basketball tomorrow?" description="regrehgsf hgfdh gnsfbdg rsthsryn s sdfg snhyrtsh sfg" votes={3} votesGoal={11}/>
-          <VotingRow name="Basketball tomorrow?" description="regrehgsf hgfdh gnsfbdg rsthsryn s sdfg snhyrtsh sfg" votes={3} votesGoal={11}/>
-          <VotingRow name="Basketball tomorrow?" description="regrehgsf hgfdh gnsfbdg rsthsryn s sdfg snhyrtsh sfg" votes={3} votesGoal={11}/> */}
+        {renderProposals()}
       </div>
   </div>
   )
