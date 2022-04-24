2b017bdfbf23fb191d24025e9fcb7977bc684353
josephbensabat <josephbensabat12@gmail.com>
2022-04-24 08:09:08 +0200

    votingrow

--- smart-dao/src/components/VotingRow.js
+++ smart-dao/src/components/VotingRow.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React,{useEffect, useState}  from "react";
 import "../style/votingRow.css"
 import moment from "moment";
+
 // class VotingRow extends React.Component {
 
 
@@ -22,15 +23,55 @@ import moment from "moment";
 // }
 
 // export default VotingRow;
-
+const fillArray = (value, len) => {
+    const arr = [];
+    for (let i = 0; i < len; i++) {
+      arr.push(value);
+    }
+    return arr;
+  }
+  
 const VotingRow = (props) => {
 
     const {timeOptions} = props;
-    console.log("tOps: " ,timeOptions)
+    const [submit, setSubmit] = useState(fillArray(0, timeOptions.length));
+
+    var votingArr = [];
+
+
+    const trackCheckbox = (e) => {
+        const checked = e.target.checked;
+        console.log("arra: ",votingArr );
+
+        if(checked){
+            //append 1
+            votingArr.push(1);
+        }
+        else{
+            //append 0
+            votingArr.push(0);
+        }
+
+
+    }
+
+    // useEffect(()=>{
+    //     console.log(submit);
+    // },[submit])
+    // const trackindex = (e) => {
+    //     let newArray = submit;
+    //     newArray[e] = !newArray[e];
+    //     setSubmit(newArray);
+    // }
 
     return (
         <div className="voting-row">
-            <button className="submit-times">Submit</button>
+            <button className="submit-times"
+
+           
+           
+            >Submit
+            </button>
             <div className="voting-about">
                 <h2 className="voting-name">{props.location}</h2>
                 <p className="voting-description"></p>
@@ -39,11 +80,11 @@ const VotingRow = (props) => {
             </div>
             <div className="voting-interact">
                 {
-                    timeOptions.map(element => {
+                    timeOptions.map((element,i) => {
                         return (
                             <div className="voting-box">
                                 <button className="approve-voting-btn">{moment(element.time).format('MM-DD HH:mm:ss')}</button>
-                                <input type="checkbox" className='time-checkbox'/>
+                                <input key={i} onChange={()=>{trackindex(i)}} type="checkbox" className='time-checkbox'/>
                                 <p className="votes-counter">{element.votes}</p>
                             </div>
                         )
